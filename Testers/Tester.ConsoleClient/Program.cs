d61f0d8a904110643ba45cd965590df019c0c4ce
Eduardo Almeida <edu.almeida1@gmail.com>
2024-01-04 17:13:01 +0000

    Fix register returnUrl bug

--- Testers/Tester.ConsoleClient/Program.cs
+++ Testers/Tester.ConsoleClient/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// discover endpoints from metadata
+using IdentityModel.Client;
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+Console.WriteLine("Finished discovery: " + disco.Json.ToString());
+
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
+
+// call api
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:6001/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+
+var responseUserInfo = await client.GetUserInfoAsync(new UserInfoRequest
+{
+    Address = disco.UserInfoEndpoint,
+    Token = tokenResponse.AccessToken
+});
+
+Console.WriteLine(responseUserInfo.Raw);
+
+/*
+ * To send the access token to the API you typically use the HTTP Authorization header. This is done using the SetBearerToken extension method:
+ */
+
+Console.ReadKey();
